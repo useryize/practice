3067d19c8206d4d2f32ce59955992fe553539738
YIZE <136076063@qq.com>
2019-07-02 11:30:13 +0800

    JavaScript深入之执行上下文栈

--- 003/index.js
+++ 003/index.js
@@ -11,8 +11,19 @@ var foo = function () {
 function foo() {
     console.log('foo1');
 }
-foo() // foo2
+// foo() // foo2
 function foo() {
     console.log('foo2')
 }
-foo() // foo2
+// foo() // foo2
+
+function fun3() {
+    console.log("fun3");
+}
+function fun2() {
+    fun3()
+}
+function fun1() {
+    fun2()
+}
+fun1()
