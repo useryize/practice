e29f125ede6dd7b7d8cf4d886a09a937bd1f21fc
hw21860065 <hw21860065@example.com>
2020-12-14 16:43:46 +0800

    type

---
+++ 008/typescript/typescript中的数据类型(上).ts
@@ -0,0 +1,26 @@
+// typescript中的数据类型 上
+
+//布尔类型
+let bool: Boolean = true;
+
+// 数字类型 number
+let num: Number = 123;
+
+// 字符串类型 String
+let str: string = 'asd';
+
+// 数组类型 Arr
+let arr: number[] = [123]; 
+let arrNUm: Array<number> = [123];
+let arrStr: Array<string> = ['123'];
+
+// 元祖类型 tuple
+let tup:[number, string] = [123, '456'];
+
+
+// 枚举类型 enum
+
+// 任意类型 any
+// null 和 undefined
+// void类型
+// never类型F
