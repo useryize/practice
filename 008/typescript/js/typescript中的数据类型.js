8932b1e1337517f4299c6c135e101a8033e9054d
hw21860065 <hw21860065@example.com>
2020-12-19 17:04:05 +0800

    typescript中的数据类型

---
+++ 008/typescript/js/typescript中的数据类型.js
@@ -0,0 +1,30 @@
+"use strict";
+// typescript中的数据类型 上
+//布尔类型
+var bool = true;
+// 数字类型 number
+var num = 789456;
+// 字符串类型 String
+var str = 'asd';
+// 数组类型 Arr
+var arr = [123];
+var arrNUm = [123];
+var arrStr = ['123'];
+// 元祖类型 tuple
+var tup = [123, '456'];
+// 枚举类型 enum
+var color;
+(function (color) {
+    color[color["a"] = 0] = "a";
+    color[color["b"] = 3] = "b";
+    color[color["c"] = 4] = "c";
+})(color || (color = {}));
+;
+var a = color.a; // 取下标
+var b = color.b; // 取本值
+var c = color.c; // 前下标加1
+// console.log(a, b, c);
+// 任意类型 any
+// null 和 undefined
+// void类型
+// never类型F
--- 008/typescript/typescript中的数据类型.ts
+++ 008/typescript/typescript中的数据类型.ts
@@ -4,23 +4,28 @@
 let bool: Boolean = true;
 
 // 数字类型 number
-let num: Number = 123;
+let num: Number = 789456;
 
 // 字符串类型 String
 let str: string = 'asd';
 
 // 数组类型 Arr
-let arr: number[] = [123]; 
+let arr: number[] = [123];
 let arrNUm: Array<number> = [123];
 let arrStr: Array<string> = ['123'];
 
 // 元祖类型 tuple
-let tup:[number, string] = [123, '456'];
-
+let tup: [number, string] = [123, '456'];
 
 // 枚举类型 enum
+enum color { a, b = 3, c };
+let a: color = color.a; // 取下标
+let b: color = color.b; // 取本值
+let c: color = color.c; // 前下标加1
+// console.log(a, b, c);
 
 // 任意类型 any
+
 // null 和 undefined
 // void类型
 // never类型F
