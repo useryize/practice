102ffac12d1a21aaf8e2d9992d0a354b09648cb8
useryize <136076063@qq.com>
2021-07-14 10:43:00 +0800

    泛型

--- 008/typescript/010_泛型.ts
+++ 008/typescript/010_泛型.ts
@@ -3,13 +3,30 @@ const init_010 = (): void => {
         name: number = 0;
         color: number = 0;
         say() {
-            console.error(this.name);
+            // console.error(this.name);
         }
     }
     let a = new A();
     a.name = 1;
     a.color = 2;
     a.say();
+
+    // class B<T = {}> {
+    //     name: T
+    //     say() {
+    //         console.error(this.name);
+    //     }
+    // }
+    // let b = new B();
+    // b.name = true;
+    // b.say();
+    
+    // function b<T>(a: T): T {
+    //     console.error(a);
+
+    //     return a;
+    // }
+    // b(false)
 }
 init_010();
 
--- 008/typescript/js/010_泛型.js
+++ 008/typescript/js/010_泛型.js
@@ -6,7 +6,7 @@ var init_010 = function () {
             this.color = 0;
         }
         A.prototype.say = function () {
-            console.error(this.name);
+            // console.error(this.name);
         };
         return A;
     }());
@@ -14,5 +14,19 @@ var init_010 = function () {
     a.name = 1;
     a.color = 2;
     a.say();
+    // class B<T = {}> {
+    //     name: T
+    //     say() {
+    //         console.error(this.name);
+    //     }
+    // }
+    // let b = new B();
+    // b.name = true;
+    // b.say();
+    // function b<T>(a: T): T {
+    //     console.error(a);
+    //     return a;
+    // }
+    // b(false)
 };
 init_010();
