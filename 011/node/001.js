660e0769ac187ab0b82493d415dae3a1ff805de4
useryize <136076063@qq.com>
2021-07-16 15:35:45 +0800

    fs

--- 011/node/001.js
+++ 011/node/001.js
@@ -1,3 +1,17 @@
 //
 let fs = require('fs');
-console.error(fs);
+
+// 写入文件
+fs.writeFile('./002fs.js', 'nihao', function (error) {
+    error && console.log(error) || console.log('ok');
+})
+
+// 删除文件
+fs.unlink('./002fs.js', function (error, data) {
+    error && console.log(error) || console.log('ok');
+
+})
+// 读取文件
+fs.readFile('./001.js', function (error, data) {
+    // console.log(data.toString());
+})
