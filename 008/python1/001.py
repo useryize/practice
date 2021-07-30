ed325ced92c7d7ea654431a9850408f898accbe6
useryize <136076063@qq.com>
2021-07-30 18:08:51 +0800

    变量
    变量

--- 008/python1/001.py
+++ 008/python1/001.py
@@ -1,1 +1,17 @@
 
+# python
+# print('变量')
+
+# 变量以及类型 数字 字符串 列表 字典 元组
+
+num = 1
+str1 = 'str'
+list1 = [1, 2, 3]
+dict1 = {'name': '132'}
+tuple1 = (1, 2, 3)
+
+print(type(num))  # <class 'int'>
+print(type(str1))  # <class 'str'>
+print(type(list1))  # <class 'list'>
+print(type(dict1))  # <class 'dict'>
+print(type(tuple1))  # <class 'tuple'>
