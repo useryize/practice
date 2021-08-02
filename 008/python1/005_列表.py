e3c48dda519a2435abd740fd60c113d7431449e4
useryize <136076063@qq.com>
2021-08-02 18:31:40 +0800

    列表
    列表

--- 008/python1/005_列表.py
+++ 008/python1/005_列表.py
@@ -5,11 +5,22 @@
 # print(listInfo[2:])  # 从第2开始取所有
 # print(listInfo[:3])  # 从第0个开始取3个
 
+# listInfo = [0, 1, 2, 3, 4, 5, 6]
+# length = len(listInfo)
+# i = 1
+# while i < length:
+#     i += 1
+# for item in listInfo:
+#     print(item)
 
-listInfo = [0, 1, 2, 3, 4, 5, 6]
-length = len(listInfo)
-i = 1
-while i < length:
-    i += 1
-for item in listInfo:
-    print(item)
+# 增加
+# append()  extend() insert()
+# listInfo = [1, 2]
+# listInfo2 = [3, 4]
+# listInfo.append(5) # [1, 2, 5]
+# listInfo.append(listInfo2) # [1, 2, [3, 4]] 整体添加
+# listInfo.extend(listInfo2)  # [1, 2, 3, 4]  逐个添加
+# listInfo.insert(0, 5)  # [1, 2, 3, 4]  带索引添加
+# print(listInfo)
+
+# 修改
