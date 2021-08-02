89180dcb3071416f8a2f49a8751d404f1d3b44aa
useryize <136076063@qq.com>
2021-08-02 16:47:04 +0800

    列表
    列表

---
+++ 008/python1/005_列表.py
@@ -0,0 +1,15 @@
+# 列表
+
+# listInfo = [0, 1, 2, 3, 4, 5, 6]
+# print(listInfo[0])
+# print(listInfo[2:])  # 从第2开始取所有
+# print(listInfo[:3])  # 从第0个开始取3个
+
+
+listInfo = [0, 1, 2, 3, 4, 5, 6]
+length = len(listInfo)
+i = 1
+while i < length:
+    i += 1
+for item in listInfo:
+    print(item)
