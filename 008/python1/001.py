b2d7085d375b27c38932652e51f3dc4f800406ae
useryize <136076063@qq.com>
2021-08-02 11:11:49 +0800

    变量
    变量

--- 008/python1/001.py
+++ 008/python1/001.py
@@ -3,20 +3,29 @@
 
 # 变量以及类型 数字 字符串 列表 字典 元组
 
-num = 1
-str1 = 'str'
-list1 = [1, 2, 3]
-dict1 = {'name': '132'}
-tuple1 = (1, 2, 3)
+# num = 1
+# str1 = 'str'
+# list1 = [1, 2, 3]
+# dict1 = {'name': '132'}
+# tuple1 = (1, 2, 3)
 
-print(type(num), 456465)  # <class 'int'>
-print(type(str1))  # <class 'str'>
-print(type(list1))  # <class 'list'>
-print(type(dict1))  # <class 'dict'>
-print(type(tuple1))  # <class 'tuple'>
+# print(type(num), 456465)  # <class 'int'>
+# print(type(str1))  # <class 'str'>
+# print(type(list1))  # <class 'list'>
+# print(type(dict1))  # <class 'dict'>
+# print(type(tuple1))  # <class 'tuple'>
 
+# 格式化输出
+# name = 'python'
+# occupations = 666
+#
+# print("%s %d" % (name, occupations))  # 格式化输出
 
-name = 'python'
-occupations = 666
+# 换行输出
+# print('1321313211 -------------')
+# print('1321313211 \n -------------')
 
-print("%s %d" % (name, occupations)) # 格式化输出
+# 输入
+
+# name = input('请输入:')
+# print('您输入的内容是:%s' % name)
