86548479c3cf5d7edddb3cf08927030fcafa8456
useryize <136076063@qq.com>
2021-08-04 16:20:44 +0800

    私有方法 命名已 __  开头
    私有方法 命名已 __  开头

--- 008/python1/010_类.py
+++ 008/python1/010_类.py
@@ -5,18 +5,25 @@ class Init01:
         print('init')
         self.a = a
         self.b = b
+        # 私有属性  命名已 __  开头
+        self.__id = 123
 
     def __str__(self):
-        print('str')
         return str(self.a)
 
     def __del__(self):
         print('del')
 
+    # 私有方法 命名已 __  开头
+    def __def_private(self):
+        print('私有方法', self.a)
+
     # 方法
     def test(self, a=0, b=0):
         print('self', self.a, self.b)
         print('def', a, b)
+        print('__id', self.__id)
+        self.__def_private()
 
 
 get_init01 = Init01(20, 20)
