9e8cc90c81dab501b1f046e1d395aef8923d0767
useryize <136076063@qq.com>
2021-08-04 15:11:14 +0800

    类
    类

---
+++ 008/python1/010_类.py
@@ -0,0 +1,15 @@
+# 定义类
+class Init01:
+    # 属性
+    def __init__(self, a=10, b=10):
+        self.a = a
+        self.b = b
+
+    # 方法
+    def test(self, a=0, b=0):
+        print('self', self.a, self.b)
+        print('def', a, b)
+
+
+get_init01 = Init01(20, 20)
+get_init01.test(10, 10)
