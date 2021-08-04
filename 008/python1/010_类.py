38ce1d61725d64ba5d1cdb7b8591fd13807932cf
useryize <136076063@qq.com>
2021-08-04 16:07:10 +0800

    类 __del__
    类 __del__

--- 008/python1/010_类.py
+++ 008/python1/010_类.py
@@ -2,11 +2,16 @@
 class Init01:
     # 属性
     def __init__(self, a=10, b=10):
+        print('init')
         self.a = a
         self.b = b
 
     def __str__(self):
+        print('str')
         return str(self.a)
+
+    def __del__(self):
+        print('del')
 
     # 方法
     def test(self, a=0, b=0):
@@ -16,4 +21,3 @@ class Init01:
 
 get_init01 = Init01(20, 20)
 get_init01.test(10, 10)
-print(get_init01)
