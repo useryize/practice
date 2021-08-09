75efcb6841e61b06e1a1cb0f276fbd1fbbee02fe
useryize <136076063@qq.com>
2021-08-09 17:23:25 +0800

    子弹移动速度
    子弹移动速度

--- 008/python/rise/001/main.py
+++ 008/python/rise/001/main.py
@@ -2,26 +2,30 @@ import pygame
 import time
 import random
 
+# 窗口宽高
+window_w = 480
+window_h = 600
+
+# 飞机的宽高
+aircraft_w = 30
+aircraft_h = 30
+
+# 子弹的宽高
+bullets_w = 10
+bullets_h = 10
+
 
 # 飞机类
 class Aircraft:
-    def __init__(self, screen, window_w, window_h, aircraft_w, aircraft_h):
+    def __init__(self, screen):
         """
         :param screen: 窗口对象
-        :param window_w: 窗口宽度
-        :param window_h: 窗口高度
-        :param aircraft_w: 飞机本身宽度
-        :param aircraft_h: 飞机本身高度
         """
         # 导入飞机图片
         self.aircraft = pygame.image.load('./icon.png')
         # 飞机坐标轴
         self.x_position = window_w / 2 - 30 / 2
         self.y_position = window_h - 100
-
-        # 飞机自身宽高
-        self.aircraft_w = aircraft_w
-        self.aircraft_h = aircraft_h
 
         # 飞机移动速度
         self.speed = 3
@@ -43,8 +47,7 @@ class Aircraft:
                 # 空格
                 if item_even.key == pygame.K_SPACE:
                     # 按空格发射子弹
-                    get_bullets = Bullets(self.screen, self.x_position, self.y_position, self.aircraft_w,
-                                          self.aircraft_h)
+                    get_bullets = Bullets(self.screen, self.x_position, self.y_position)
 
                     # 保存每一颗子弹
                     self.bulletsList.append(get_bullets)
@@ -64,6 +67,7 @@ class Aircraft:
         if key_pressed[pygame.K_d] or key_pressed[pygame.K_RIGHT]:
             self.x_position += self.speed
 
+        # 子弹触发频率高 改为按键触发
         # if key_pressed[pygame.K_SPACE]:
         #     # 按空格发射子弹
         #     get_bullets = Bullets(self.screen, self.x_position, self.y_position, self.aircraft_w, self.aircraft_h)
@@ -79,48 +83,44 @@ class Aircraft:
 
         # 遍历子弹列表 并将每一颗子弹显示
         for item in self.bulletsList:
-            # 没一个item都是一个子弹类的实例
-            item.auto_moving()
+            # 每一个item都是一个子弹类的实例
             item.display_bullets()
 
 
 # 子弹类
 class Bullets:
-    def __init__(self, screen, x, y, z, w):
+    def __init__(self, screen, x, y):
         """
         :param screen: 窗口对象
-        :param x: x坐标
-        :param y: y坐标
+        :param x: 飞机x坐标
+        :param y: 飞机y坐标
         """
         self.bullets_img = pygame.image.load('icon01.jpg')
-        self.x = x + z / 2 - 5
-        self.y = y - 10
         self.screen = screen
-        self.speed = 2
 
-    # 子弹自动移动
-    def auto_moving(self):
-        self.y -= self.speed
+        # 子弹坐标
+        self.x = x + aircraft_w / 2 - bullets_w / 2
+        self.y = y - 10
+
+        # 子弹移动速度
+        self.speed = 2
 
     # 显示子弹
     def display_bullets(self):
         self.screen.blit(self.bullets_img, (self.x, self.y))
+        # 子弹显示后 下次Y值减移动速度
+        self.y -= self.speed
 
 
 def main():
     pygame.init()
-    # 窗口宽高
-    window_w = 480
-    window_h = 480
-    # 飞机的宽高
-    aircraft_w = 30
-    aircraft_h = 30
+
     # 创建窗口
     screen = pygame.display.set_mode((window_w, window_h))
     # 创建窗口标题
     pygame.display.set_caption('easy')
     # 获取飞机类
-    get_aircraft = Aircraft(screen, window_w, window_h, aircraft_w, aircraft_h)
+    get_aircraft = Aircraft(screen)
 
     while True:
         # 执行飞机监听事件
