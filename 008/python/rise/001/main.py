95cfc9560585bf5f3a4e3fae2aeb1c87ced0811a
useryize <136076063@qq.com>
2021-08-09 15:23:06 +0800

    发射子弹时 子弹的位置定位
    发射子弹时 子弹的位置定位

--- 008/python/rise/001/main.py
+++ 008/python/rise/001/main.py
@@ -4,15 +4,31 @@ import time
 
 # 飞机类
 class Aircraft:
-    def __init__(self, screen, window_w, window_h):
+    def __init__(self, screen, window_w, window_h, aircraft_w, aircraft_h):
+        """
+
+        :param screen: 窗口对象
+        :param window_w: 窗口宽度
+        :param window_h: 窗口高度
+        :param aircraft_w: 飞机本身宽度
+        :param aircraft_h: 飞机本身高度
+        """
         # 导入飞机图片
         self.aircraft = pygame.image.load('./icon.png')
         # 飞机坐标轴
-        self.x_position = window_h / 2 - 30 / 2
-        self.y_position = window_w - 100
+        self.x_position = window_w / 2 - 30 / 2
+        self.y_position = window_h - 100
+
+        # 飞机自身宽高
+        self.aircraft_w = aircraft_w
+        self.aircraft_h = aircraft_h
+
         # 飞机移动速度
         self.speed = 3
         self.screen = screen
+
+        # 接受发射的子弹
+        self.bulletsList = []
 
     # 监听键盘事件
     def get_pressed_aircraft(self):
@@ -37,13 +53,13 @@ class Aircraft:
         # print('右')
         if key_pressed[pygame.K_d] or key_pressed[pygame.K_RIGHT]:
             self.x_position += self.speed
+
         if key_pressed[pygame.K_SPACE]:
             # 按空格发射子弹
-            # self.screen.fill((30, 8, 7))
-            # self.screen.blit(self.aircraft, (0, 0))
-            # 将飞机导入到窗口上
-            get_bullets = Bullets(self.screen, self.x_position, self.y_position)
-            get_bullets.display_bullets()
+            get_bullets = Bullets(self.screen, self.x_position, self.y_position, self.aircraft_w, self.aircraft_h)
+
+            # 保存每一颗子弹
+            self.bulletsList.append(get_bullets)
 
     def display_aircraft(self):
         # 设置背景颜色
@@ -51,32 +67,44 @@ class Aircraft:
         # 将飞机导入到窗口上
         self.screen.blit(self.aircraft, (self.x_position, self.y_position))
 
+        # 遍历子弹列表 并将每一颗子弹显示
+        for item in self.bulletsList:
+            # 没一个item都是一个子弹类的实例
+            item.display_bullets()
+
 
 # 子弹类
 class Bullets:
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, x, y, z, w):
+        """
+        :param screen: 窗口对象
+        :param x: x坐标
+        :param y: y坐标
+        """
         self.bullets_img = pygame.image.load('icon01.jpg')
-        self.x = 0
-        self.y = 0
+        self.x = x + z/2 - 5
+        self.y = y - 10
         self.screen = screen
 
     # 显示子弹
     def display_bullets(self):
-        self.screen.blit(self.bullets_img, (0, 0))
+        self.screen.blit(self.bullets_img, (self.x, self.y))
 
 
 def main():
     pygame.init()
-    # 窗口宽度
+    # 窗口宽高
     window_w = 480
-    # 窗口高度
     window_h = 480
+    # 飞机的宽高
+    aircraft_w = 30
+    aircraft_h = 30
     # 创建窗口
     screen = pygame.display.set_mode((window_w, window_h))
     # 创建窗口标题
     pygame.display.set_caption('easy')
     # 获取飞机类
-    get_aircraft = Aircraft(screen, window_w, window_h)
+    get_aircraft = Aircraft(screen, window_w, window_h, aircraft_w, aircraft_h)
 
     while True:
         # 执行飞机监听事件
