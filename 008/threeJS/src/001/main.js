8d06d7970e7a08f34acaf84aa86e3629b32eedca
useryize <136076063@qq.com>
2022-09-21 16:36:42 +0800

    轨道控制器
    轨道控制器

--- 008/threeJS/src/001/main.js
+++ 008/threeJS/src/001/main.js
@@ -1,17 +1,9 @@
 import * as THREE from 'three'
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' // 导入轨道控制器
 // 创建场景
 const scene = new THREE.Scene()
 
 // 创建相机
-/**
- * 透视相机（PerspectiveCamera）
- * PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
- * fov — 摄像机视锥体垂直视野角度
- * aspect — 摄像机视锥体长宽比
- * near — 摄像机视锥体近端面
- * far — 摄像机视锥体远端面
- */
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
 
 // 设置相机位置
@@ -22,8 +14,8 @@ scene.add(camera)
 
 // 添加物体
 // 创建几何体
-const boxGeometry  = new THREE.BoxGeometry(1,1,1) // x y z 长宽高
-const meshBasicMaterial = new THREE.MeshBasicMaterial({color: 0xffcccc}) // 材质
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1) // x y z 长宽高
+const meshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xffcccc }) // 材质
 
 // 根据几何体和材质创建物体
 const mesh = new THREE.Mesh(boxGeometry, meshBasicMaterial)
@@ -36,6 +28,18 @@ const render = new THREE.WebGL1Renderer();
 
 // 设置渲染器尺寸
 render.setSize(window.innerWidth, window.innerHeight)
-
 document.body.append(render.domElement)
-render.render(scene, camera)
+
+// 使用渲染器通过相机将场景渲染出来
+// render.render(scene, camera)
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, render.domElement)
+
+// 封装渲染函数
+function renderFun() {
+    render.render(scene, camera)
+    // 渲染下一帧时执行函数 window.requestAnimationFrame(callback); Html5新增神器
+    requestAnimationFrame(renderFun)
+}
+renderFun()
