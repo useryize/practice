464ad4e2e0334d50f13e6148883223e9a5f066c7
useryize <136076063@qq.com>
2022-09-22 16:06:57 +0800

    封装物体移动
    封装物体移动

--- 008/threeJS/src/001/main.js
+++ 008/threeJS/src/001/main.js
@@ -18,10 +18,15 @@ const boxGeometry = new THREE.BoxGeometry(1, 1, 1) // x y z 长宽高
 const meshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xffcccc }) // 材质
 
 // 根据几何体和材质创建物体
-const mesh = new THREE.Mesh(boxGeometry, meshBasicMaterial)
+const cube1 = new THREE.Mesh(boxGeometry, meshBasicMaterial)
+const cube2 = new THREE.Mesh(boxGeometry, meshBasicMaterial)
+const cube3 = new THREE.Mesh(boxGeometry, meshBasicMaterial)
 
 // 将物体添加到场景中
-scene.add(mesh)
+scene.add(cube1)
+scene.add(cube2)
+scene.add(cube3)
+
 
 // 初始化渲染器
 const render = new THREE.WebGL1Renderer();
@@ -36,10 +41,44 @@ document.body.append(render.domElement)
 // 创建轨道控制器
 const controls = new OrbitControls(camera, render.domElement)
 
+// 添加坐标辅佐器
+const AxesHelper = new THREE.AxesHelper(10)
+
+// 将辅助器添加到场景中
+scene.add(AxesHelper)
+
+
 // 封装渲染函数
 function renderFun() {
+    cubeMoving()
     render.render(scene, camera)
     // 渲染下一帧时执行函数 window.requestAnimationFrame(callback); Html5新增神器
     requestAnimationFrame(renderFun)
 }
+
+// 封装物体移动
+let xType = 1 // 移动方向
+const num = 0.03 // 移动速度
+const num2 = 10 // 反向点
+const num3 = 0 // 反向点
+const cubeMoving = (arr) => {
+    if (xType === 1) {
+        cube1.position.x += num
+        cube2.position.y += num
+        cube3.position.z += num
+    }
+    if (xType === 2) {
+        cube1.position.x -= num
+        cube2.position.y -= num
+        cube3.position.z -= num
+    }
+    if (cube1.position.x > num2 || cube2.position.y > num2 || cube3.position.z > num2) {
+        xType = 2
+    }
+    if (cube1.position.x < num3 || cube2.position.y < num3 || cube3.position.z < num3) {
+        xType = 1
+    }
+
+}
+
 renderFun()
