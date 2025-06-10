45ee543a839dffb5781938164c79127592cea6d9
useryize <136076063@qq.com>
2025-06-10 15:22:09 +0800

    移除元素

--- 011/001/index.js
+++ 011/001/index.js
@@ -52,3 +52,32 @@ const fun = (nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3) => {
 }
 
 console.log(fun([1, 2, 3, 0, 0, 0], 3, [2, 3, 5, 6, 7, 8, 9], 6))
+
+
+
+// 移除元素
+// 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。
+
+// 假设 nums 中不等于 val 的元素数量为 k，要通过此题，您需要执行以下操作：
+
+// 更改 nums 数组，使 nums 的前 k 个元素包含不等于 val 的元素。nums 的其余元素和 nums 的大小并不重要。
+// 返回 k。
+
+const fun2 = (list, val) => {
+    if (!list) return
+    let newList = [];
+    let newList2 = [];
+    let k = 0
+    list.forEach(i => {
+        if (i !== val) {
+            newList = [...newList, i]
+            k++
+        } else {
+            newList2 = [...newList2, '_']
+        }
+
+    });
+
+    return { k, newList: [...newList, ...newList2] }
+}
+console.log(fun2([0, 1, 2, 2, 3, 0, 4, 2], 2))
